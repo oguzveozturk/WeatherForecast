f0a933e982b216ee2f736ad729d6aa3f24df54cf
Oğuz ÖZTÜRK <oguzveozturk@gmail.com>
2023-11-17 13:44:42 +0300

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Oğuz Öztürk on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
